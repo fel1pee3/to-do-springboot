5ead810c0fb950e8cd04fe2985031d2771829232
Felipe Maia <132314205+fel1pee3@users.noreply.github.com>
2024-12-07 17:01:38 -0300

    definição do dominio Repository OK

---
+++ src/main/java/br/com/felipemaia/todolist/Repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package br.com.felipemaia.todolist.Repository;
+
+import br.com.felipemaia.todolist.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+}
