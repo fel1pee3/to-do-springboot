69c3bb32b6e16623aa9d7548adb90a43395f6168
Felipe Maia <132314205+fel1pee3@users.noreply.github.com>
2024-12-07 16:56:42 -0300

    definição do dominio Entidade OK

---
+++ src/main/java/br/com/felipemaia/todolist/entity/Todo.java
@@ -0,0 +1,24 @@
+package br.com.felipemaia.todolist.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "todos")
+@Data
+
+public class Todo {
+
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String decricao;
+    private boolean realizado;
+    private int prioridade;
+
+}
