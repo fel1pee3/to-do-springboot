63c09c3213524270688408da918187f1b96b5fcf
Felipe Maia <132314205+fel1pee3@users.noreply.github.com>
2024-12-07 19:21:39 -0300

    erro de corrigido e criação de tabela OK

--- src/main/java/br/com/felipemaia/todolist/entity/Todo.java
+++ src/main/java/br/com/felipemaia/todolist/entity/Todo.java
@@ -1,24 +1,62 @@
 package br.com.felipemaia.todolist.entity;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;  // Correção aqui: importando de jakarta.persistence.Id
 import jakarta.persistence.Table;
-import lombok.Data;
 
 @Entity
 @Table(name = "todos")
-@Data
-
 public class Todo {
 
-    @id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
-    private String decricao;
+    private String descricao;  // Corrigido para "descricao"
     private boolean realizado;
     private int prioridade;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
 }
+
